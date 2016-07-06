a882af64528c683fe6ef49c533e261cf559720a6
Johan Li <johan@johanli.com>
2016-07-06 12:39:21 +0200

    include public/img for now, as imagemin is skipping files on production

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,29 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
-var changed = require('gulp-changed');
-var imagemin = require('gulp-imagemin');
-var nodemon = require('gulp-nodemon');
-
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('sass', function () {
-    gulp.src('./src/sass/base.scss')
+    gulp.src('src/sass/base.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('./src/js/*.js')
+    return gulp.src('src/js/*.js')
         .pipe(concat('scripts.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
+        .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('imagemin', function() {
-    var imgSource = './src/img/**/*';
-    var imgDestination = './public/img';
+    var imgSource = 'src/img/**/*';
+    var imgDestination = 'public/img';
 
     gulp.src(imgSource)
         .pipe(changed(imgDestination))
@@ -33,8 +32,9 @@ gulp.task('imagemin', function() {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./src/sass/*.scss', ['sass']);
-    gulp.watch('./src/js/*.js', ['scripts']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/img/*.*', ['imagemin']);
 });
 
 gulp.task('start', function () {
@@ -46,6 +46,6 @@ gulp.task('start', function () {
     })
 });
 
-gulp.task('default', ['sass', 'imagemin', 'scripts'], function() {});
+gulp.task('default', ['sass', 'scripts', 'imagemin'], function() {});
 
 gulp.task('dev', ['watch', 'start'], function() {});
