18ee5df59f3f64802395f0a1dc06a5a059928550
Johan Li <johan@johanli.com>
2017-02-08 00:25:23 +0100

    imagemin for gifs

--- imagemin.js
+++ imagemin.js
@@ -1,9 +1,11 @@
 const imagemin = require('imagemin');
+const gifsicle = require('imagemin-gifsicle');
 const mozjpeg = require('imagemin-mozjpeg');
 const optipng = require('imagemin-optipng');
 
-imagemin(['src/img/*.{jpg,png,ico}'], 'public/img/', {
+imagemin(['src/img/*.{gif,jpg,png,ico}'], 'public/img/', {
     plugins: [
+        gifsicle(),
         mozjpeg({
             quality: 85,
             progressive: false
