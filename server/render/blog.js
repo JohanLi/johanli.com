d8f65ba6cb9c06638ebd778a7a41ea52a153a479
Johan Li <johan@johanli.com>
2017-09-30 14:46:11 +0200

    fix server rendering logic

--- server/render/blog.js
+++ server/render/blog.js
@@ -2,14 +2,13 @@ import { matchPath } from 'react-router-dom';
 import blogModel from '../api/models/blog';
 
 export default async (req) => {
+  const { entries, archive, totalPages } = await blogModel.getPage(1);
   const blog = {
     entries: {},
     pages: {},
-    archive: await blogModel.getArchive(),
-    totalPages: await blogModel.getTotalPages(),
+    archive,
+    totalPages,
   };
-
-  let entries = await blogModel.getPage(1);
 
   entries.forEach((entry) => {
     blog.entries[entry.url] = entry;
@@ -27,18 +26,23 @@ export default async (req) => {
 
   const blogPageRequested = !isNaN(match.params.pageOrUrlKey);
 
+  let requestedBlog = {
+    entries: [],
+  };
+
   if (blogPageRequested) {
-    entries = await blogModel.getPage(match.params.pageOrUrlKey);
+    requestedBlog = await blogModel.getPage(match.params.pageOrUrlKey);
   } else {
-    entries = await blogModel.getByUrlKey(match.params.pageOrUrlKey);
+    requestedBlog = await blogModel.getByUrlKey(match.params.pageOrUrlKey);
   }
 
-  entries.forEach((entry) => {
+  const requestedEntries = requestedBlog.entries;
+  requestedEntries.forEach((entry) => {
     blog.entries[entry.url] = entry;
   });
 
   if (blogPageRequested) {
-    blog.pages[match.params.pageOrUrlKey] = entries.map(entry => entry.url);
+    blog.pages[match.params.pageOrUrlKey] = requestedEntries.map(entry => entry.url);
   }
 
   return blog;
