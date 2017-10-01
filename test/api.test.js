4787e41d5b0e6c07a02668b6bc391483854e732f
Johan Li <johan@johanli.com>
2017-10-01 09:29:58 +0200

    update api tests

--- test/api.test.js
+++ test/api.test.js
@@ -5,6 +5,35 @@ import apiOnly from '../server/apiOnly';
 
 chai.use(chaiHttp);
 
+const blog = {
+  entries: {
+    id: '',
+    url: '',
+    title: '',
+    html: '',
+    excerpt: '',
+    published: '',
+    side_project_id: '',
+  },
+  archive: {
+    entries: '',
+    year: '',
+  },
+  totalPages: '',
+};
+
+const sideProjects = {
+  id: '',
+  name: '',
+  description: '',
+  homepage_url: '',
+  github_url: '',
+  image_url: '',
+  state: '',
+  blogEntries: '',
+};
+
+
 describe('Api', () => {
   it('Responds to GET /api/blog/:pageOrUrlKey for page', (done) => {
     chai.request(apiOnly)
@@ -12,13 +41,10 @@ describe('Api', () => {
       .end((err, res) => {
         assert.equal(err, null);
         assert.equal(res.statusCode, 200);
-        assert.hasAllKeys(
-          res.body,
-          [
-            'entries',
-            'pagination',
-          ],
-        );
+        assert.hasAllKeys(res.body, Object.keys(blog));
+        assert.hasAllKeys(res.body.entries[0], Object.keys(blog.entries));
+        assert.hasAllKeys(res.body.archive[0], Object.keys(blog.archive));
+        assert.equal(Number.isInteger(res.body.totalPages), true);
         done();
       });
   });
@@ -29,29 +55,11 @@ describe('Api', () => {
       .end((err, res) => {
         assert.equal(err, null);
         assert.equal(res.statusCode, 200);
-        assert.hasAllKeys(
-          res.body,
-          [
-            'entries',
-          ],
-        );
-        done();
-      });
-  });
-
-  it('Responds to GET /api/blog/archive', (done) => {
-    chai.request(apiOnly)
-      .get('/api/blog/archive')
-      .end((err, res) => {
-        assert.equal(err, null);
-        assert.equal(res.statusCode, 200);
-        assert.hasAllKeys(
-          res.body[0],
-          [
-            'year',
-            'entries',
-          ],
-        );
+        assert.hasAllKeys(res.body, Object.keys(blog));
+        assert.hasAllKeys(res.body.entries[0], Object.keys(blog.entries));
+        assert.hasAllKeys(res.body.archive[0], Object.keys(blog.archive));
+        assert.equal(Number.isInteger(res.body.totalPages), true);
+        assert.equal(res.body.entries.length, 1);
         done();
       });
   });
@@ -62,19 +70,7 @@ describe('Api', () => {
       .end((err, res) => {
         assert.equal(err, null);
         assert.equal(res.statusCode, 200);
-        assert.hasAllKeys(
-          res.body[0],
-          [
-            'id',
-            'name',
-            'description',
-            'homepage_url',
-            'github_url',
-            'image_url',
-            'state',
-            'blogEntries',
-          ],
-        );
+        assert.hasAllKeys(res.body[0], Object.keys(sideProjects));
         done();
       });
   });
